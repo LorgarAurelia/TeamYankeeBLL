4a699fd7bcddb676a9da5e507f912e2e5594af33
Malikrot <Malikrot@DESKTOP-G1962IL>
2023-02-07 20:22:19 +0600

    Add Dependency

---
+++ Extension/DependencyInjectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamYankeeBLL.SQL;
+
+namespace TeamYankeeBLL.Extension
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddRostersServices(this IServiceCollection services)
+        {
+            services.AddTransient<IDataParser, DataParser>();
+            services.AddTransient<IRosterRepository, RosterRepository>();
+            services.AddTransient<IFormationsRepository, FormationRepository>();
+            services.AddTransient<IOptionsRepository, OptionsRepository>();
+            services.AddTransient<ISpecificationRepository, SpecificationRepository>();
+            services.AddTransient<IUnitsRepository, UnitRepository>();
+
+            return services;
+        }
+    }
+}
